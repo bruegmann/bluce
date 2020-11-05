8a14bf2d2850e340b83325c99d1fc1ed4d3a0c41
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-11-05 12:23:24 +0100

    Many improvements

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -25,3 +25,24 @@ import "../../node_modules/bootstrap/js/dist/modal.js";
 document.getElementById("toggle-dark").addEventListener("click", () => {
     document.querySelector("body").classList.toggle("dark")
 })
+
+const stylesheet = document.getElementById("stylesheet")
+const title = document.getElementById("title")
+const ownStyle = "assets/css/starter.css"
+
+const toggleStylesheet = () => {
+    if (stylesheet.getAttribute("href") === ownStyle) {
+        stylesheet.setAttribute("href", "https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css")
+        title.innerHTML = "Bootstrap"
+    }
+    else {
+        stylesheet.setAttribute("href", ownStyle)
+        title.innerHTML = "Bluce"
+    }
+}
+
+document.getElementById("toggle-stylesheet").addEventListener("click", toggleStylesheet)
+
+document.getElementById("show-test-components").addEventListener("click", () => {
+    document.getElementById("test-components").classList.toggle("d-none")
+})
--- convert-colors.js
+++ convert-colors.js
@@ -1,20 +1,19 @@
 const input = /*scss*/`
-$white:    hsl(0, 0%, 100%);
-$gray-100: hsl(210, 17%, 98%);
-$gray-200: hsl(210, 16%, 93%);
-$gray-300: hsl(210, 14%, 89%);
-$gray-400: hsl(210, 14%, 83%);
-$gray-500: hsl(210, 11%, 71%);
-$gray-600: hsl(208, 7%, 46%);
-$gray-700: hsl(210, 9%, 31%);
-$gray-800: hsl(210, 10%, 23%);
-$gray-900: hsl(210, 11%, 15%);
-$black:    hsl(0, 0%, 0%);
+$blue:    hsl(211, 100%, 50%);
+$indigo:  hsl(263, 90%, 51%);
+$purple:  hsl(261, 51%, 51%);
+$pink:    hsl(332, 79%, 58%);
+$red:     hsl(354, 70%, 54%);
+$orange:  hsl(27, 98%, 54%);
+$yellow:  hsl(45, 100%, 51%);
+$green:   hsl(134, 61%, 41%);
+$teal:    hsl(162, 73%, 46%);
+$cyan:    hsl(188, 78%, 41%);
 `
 
 const lines = input.split("\n")
 let outputLines = []
-
+let scssVarLines = []
 
 for (let i = 0; i < lines.length; i++) {
     const line = lines[i].trim()
@@ -31,7 +30,11 @@ for (let i = 0; i < lines.length; i++) {
         outputLines.push(`--${res[0]}-l: ${res[4]};`)
         outputLines.push(`--${res[0]}: hsl(var(--${res[0]}-h), var(--${res[0]}-s), var(--${res[0]}-l));`)
         outputLines.push("")
+
+        scssVarLines.push(`$${res[0]}: var(--${res[0]});`)
     }
 }
 
 console.log(outputLines.join("\n"))
+console.log("/*---------------*/\n")
+console.log(scssVarLines.join("\n"))
