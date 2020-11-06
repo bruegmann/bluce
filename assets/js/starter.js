d9e8a24ec6abf587a63a0e1f4d6c4b48c061324c
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-11-06 08:08:07 +0100

    Fix

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -18,8 +18,8 @@
 // If you're importing tooltips or popovers, be sure to include our Popper.js dependency
 // import "../../node_modules/popper.js/dist/popper.min.js";
 
-import "../../node_modules/bootstrap/js/dist/util.js";
-import "../../node_modules/bootstrap/js/dist/modal.js";
+// import "../../node_modules/bootstrap/js/dist/util.js";
+// import "../../node_modules/bootstrap/js/dist/modal.js";
 
 
 document.getElementById("toggle-dark").addEventListener("click", () => {
@@ -42,7 +42,3 @@ const toggleStylesheet = () => {
 }
 
 document.getElementById("toggle-stylesheet").addEventListener("click", toggleStylesheet)
-
-document.getElementById("show-test-components").addEventListener("click", () => {
-    document.getElementById("test-components").classList.toggle("d-none")
-})
