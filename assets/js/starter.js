fcfec1cb75a3a1061a4310988c35c19dd8a75f50
lgk-bsw <64956470+lgk-bsw@users.noreply.github.com>
2020-11-06 09:26:55 +0100

    Seperated and commented code

--- assets/js/starter.js
+++ assets/js/starter.js
@@ -1,29 +1,11 @@
-// Importing JavaScript
-//
-// You have two choices for including Bootstrap's JS files—the whole thing,
-// or just the bits that you need.
+const prefersDarkScheme = window.matchMedia("(prefers-color-scheme: dark)")
+prefersDarkScheme.addEventListener("change", () => {
+    document.body.classList.remove("light-theme")
+    document.body.classList.remove("dark-theme")
+})
 
-
-// Option 1
-//
-// Import Bootstrap's bundle (all of Bootstrap's JS + Popper.js dependency)
-
-// import "../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
-
-
-// Option 2
-//
-// Import just what we need
-
-// If you're importing tooltips or popovers, be sure to include our Popper.js dependency
-// import "../../node_modules/popper.js/dist/popper.min.js";
-
-// import "../../node_modules/bootstrap/js/dist/util.js";
-// import "../../node_modules/bootstrap/js/dist/modal.js";
-
-
-document.getElementById("toggle-dark").addEventListener("click", () => {
-    document.querySelector("body").classList.toggle("dark")
+document.getElementById("toggle-themed").addEventListener("click", () => {
+    document.querySelector("body").classList.toggle(prefersDarkScheme.matches ? "light-theme" : "dark-theme")
 })
 
 const stylesheet = document.getElementById("stylesheet")
